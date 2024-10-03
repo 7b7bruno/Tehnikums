0ed9e9d51600940102c7d1f2ad82ccae1762bba9
Bruno <your@email.example>
2024-10-03 09:18:02 +0300

    db design; +join and delete exercises

---
+++ 4users/1.sql
@@ -0,0 +1,7 @@
+select * from mysql.user;
+
+create user php_app@localhost identified by "1234";
+drop user "php_app";
+
+grant all privileges on sql_store.* to php_app@localhost;
+flush privileges;
